c62fbcd06d7a40a6e45c84d4563170e218dd03d7
Kurtis Lunz <Kurtis987@gmail.com>
2017-12-21 11:02:28 -0500

    Navigation plus wireframes

---
+++ js/faithMoves.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    selectStyle();
+
+    /* check to see if we need restyle on
+       window resize */
+    $(window).resize(selectStyle);
+
+    $("a[href='prayer.html']").hover(function(){
+			$("#prayer-link-img").replaceWith("<img id='prayer-link-img' src='img/prayerHover.png' alt='prayer icon' height='20' width='20'>");
+		}, function(){
+			$("#prayer-link-img").replaceWith("<img id='prayer-link-img' src='img/prayer.png' alt='prayer icon' height='20' width='20'>");
+	});
+
+});
+
+
+    /* use css of an element to see
+       which media query we're using
+       then select the correct class
+       once we determine the screen size */
+function selectStyle() {
+    //if ($(".caret").css("position") == "absolute" ) {
+    if($(window).width() <= 767) {
+    	//screen is < 768px switch to glyphicon-plus
+        $("span.caret").removeClass("caret").addClass("glyphicon").addClass("glyphicon-plus");
+    }
+    else {
+    //if($(".glyphicon-plus").css("left") == "2px") {
+    	//screen is >= 768 switch to caret
+    	$("span.glyphicon-plus").removeClass("glyphicon").removeClass("glyphicon-plus").addClass("caret");
+    	
+    }
+    if($(window).width() >= 1071) {
+    	$("#logo").replaceWith("<img id='logo' src='img/FaithMovesLogoLarge.png' alt='Faith Moves Logo' width='444' height='105'>");
+    }
+    else {
+    	$("#logo").replaceWith("<img id='logo' src='img/FaithMovesLogoSmall.png' alt='Faith Moves Logo' width='185' height='41'>");
+    }
+}
