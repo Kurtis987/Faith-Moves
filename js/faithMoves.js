97dfadd5592086e8481e26b22f01a618766aec86
Kurtis Lunz <Kurtis987@gmail.com>
2018-02-03 18:32:51 -0500

    commented out log statements

--- js/faithMoves.js
+++ js/faithMoves.js
@@ -12,50 +12,15 @@ $(document).ready(function() {
 
 });
 
-
-
-/*function selectStyle() { 
-    //responsive width fix credit to forsvunnet on https://github.com/eddiemachado-zz/bones/issues/468
-    var responsive_width = window.innerWidth || $(window).width();
-
-    if(responsive_width <= 496)
-    {
-        $('#logo_live365').addClass('img-logo_live365-Left');
-        $('.player').addClass('player-small-screen');
-    }
-    else
-    {
-        $('#logo_live365').removeClass('img-logo_live365-Left');
-        $('.player').removeClass('player-small-screen');
-    }
-    if(responsive_width <= 767) {
-    	//screen is < 768px switch to glyphicon-plus
-        $("span.caret").removeClass("caret").addClass("glyphicon").addClass("glyphicon-plus");
-    }
-    else { 
-    	//screen is >= 768 switch to caret
-    	$("span.glyphicon-plus").removeClass("glyphicon").removeClass("glyphicon-plus").addClass("caret");
-    	
-    }
-    if(responsive_width <= 804 && responsive_width > 767)
-    {
-        $('.player').addClass('player-avoid-menu');
-    }
-    else
-    {
-        $('.player').removeClass('player-avoid-menu');
-    }
-}*/
-
 function selectStyle() { 
     //bootstrap seems to size accourding to innerWidth it is also the width Chrome reports
     var responsive_width = window.innerWidth;
     //$(window).width() is the width used for my custom breakpoint and is the actual width
-    console.log("responsive: "+ responsive_width);
-    console.log("window: "+ $(window).width());
+    //console.log("responsive: "+ responsive_width);
+    //console.log("window: "+ $(window).width());
 
     var browser = navigator.userAgent;
-    if(browser.search("NET CLR") != -1){
+    /*if(browser.search("NET CLR") != -1){
         console.log("ie");
     }
     else if(browser.search("Edge") != -1 && $(window).width() <= 503 )
@@ -71,7 +36,7 @@ function selectStyle() {
         console.log(browser.search("Edge"));
         console.log(browser);
         console.log("Edge");
-    }
+    }*/
 
     if(browser.search("Edge") == -1 && browser.search("NET CLR") == -1 && responsive_width < 521 
         || browser.search("Edge") != -1 && $(window).width() <= 503 || 
